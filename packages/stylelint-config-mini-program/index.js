223c09b936cbbf9997108e6ca0c658c05fb77583
Leedom <jld1221@163.com>
2024-04-18 22:38:36 +0800

    feat(mini-program): ignore font-family

--- packages/stylelint-config-mini-program/index.js
+++ packages/stylelint-config-mini-program/index.js
@@ -3,6 +3,7 @@ module.exports = {
     {
       files: ['*.acss', '*.wxss'],
       rules: {
+        'font-family-no-missing-generic-family-keyword': null,
         'selector-type-no-unknown': null,
         'unit-no-unknown': [true, { ignoreUnits: ['rpx'] }],
       },
